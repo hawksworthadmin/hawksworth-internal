04df240e8f3fcd8b0a0a37a0a958c9a68b1f12bc
Emeka Ifeora <34845973+Emyboy@users.noreply.github.com>
2024-09-24 12:50:40 +0100

    update

--- src/app/slice-simulator/page.js
+++ src/app/slice-simulator/page.js
@@ -2,16 +2,16 @@ import {
   SliceSimulator,
   getSlices,
 } from "@slicemachine/adapter-next/simulator";
-import { SliceZone } from "@prismicio/react";
+// import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices";
+// import { components } from "../../slices";
 
 export default function SliceSimulatorPage({ searchParams }) {
   const slices = getSlices(searchParams.state);
 
   return (
     <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
+      {/* <SliceZone slices={slices} components={components} /> */}
     </SliceSimulator>
   );
 }
